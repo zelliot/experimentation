faf610194144b705079d0c3de478728dee7faf37
Elliot Braham <elliot@Elliots-MacBook-Pro.local>
2020-11-30 19:13:27 +1100

    A quick example using Get for state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,89 +1,87 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 /// Don't forget to import the stepper.dart class.
 import 'package:im_stepper/stepper.dart';
 
-void main() {
-  runApp(DotStepperDemo());
+void main() => runApp(GetMaterialApp(home: DotStepperDemo()));
+
+class Controller extends GetxController {
+  var activeStep = 1.obs;
 }
 
-class DotStepperDemo extends StatefulWidget {
-  @override
-  _DotStepperDemo createState() => _DotStepperDemo();
-}
-
-class _DotStepperDemo extends State<DotStepperDemo> {
+class DotStepperDemo extends StatelessWidget {
   // MUST DEFINE THE FOLLOWING THREE VARIABLES.
   // activeStep must be Set to 1 and NOT to 0
-  int activeStep = 1;
 
-  int lowerBound = 1;
-  int upperBound = 5;
+  // int lowerBound = 1;
+  // int upperBound = 5;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('DotStepper Example'),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  previousButton(),
-                  Expanded(
-                    child: DotStepper(
-                      dotCount: 5,
-                      activeStep: activeStep,
-                      lowerBound: (bound) => lowerBound = bound,
-                      upperBound: (bound) => upperBound = bound,
-                      indicatorEffect: IndicatorEffect.jump,
-                    ),
-                  ),
-                  nextButton(),
-                ],
-              ),
-              Expanded(
-                child: FittedBox(
-                  child: Text(
-                    '$activeStep',
+    final controller = Get.put(Controller());
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('DotStepper Example'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                previousButton(),
+                Expanded(
+                  child: Obx(() => DotStepper(
+                        dotCount: 5,
+                        activeStep: controller.activeStep.value,
+                        // TODO: You actually don't need lowerBound because it will always be 1
+                        lowerBound: (bound) => 1,
+                        // TODO: You actually don't need upperBound because it will always be = dotCount
+                        upperBound: (bound) => 5,
+                        indicatorEffect: IndicatorEffect.jump,
+                      )),
+                ),
+                nextButton(),
+              ],
+            ),
+            Expanded(
+              child: FittedBox(
+                child: Obx(
+                  () => Text(
+                    '${controller.activeStep.value}',
                   ),
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
   }
 
-  Widget nextButton() {
+  nextButton() {
+    final controller = Get.put(Controller());
     return ElevatedButton(
       child: Text('Next'),
       onPressed: () {
         // Constraint is a must.
-        if (activeStep < upperBound) {
-          setState(() {
-            activeStep++;
-          });
-        }
+        // if (controller.activeStep.value < upperBound) {
+        controller.activeStep.value++;
+        // }
       },
     );
   }
 
   Widget previousButton() {
+    final controller = Get.put(Controller());
     return ElevatedButton(
       child: Text('Previous'),
       onPressed: () {
         // Constraint is a must.
-        if (activeStep > lowerBound) {
-          setState(() {
-            activeStep--;
-          });
-        }
+        // if (controller.activeStep.value > lowerBound) {
+        controller.activeStep.value--;
+        // }
       },
     );
   }
